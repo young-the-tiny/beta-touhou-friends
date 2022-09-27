64f631a4699ea1ab14c1bc38f8531e31e9905e1e
madme <tatrantuyen@gmail.com>
2022-09-27 18:33:57 +0700

    Updates

---
+++ precache-manifest.c30f2396f2046bbb79af83bb71e32bd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5581d36e76261ad5e333",
+    "url": "/beta-touhou-friends/static/css/main.d8ded0f1.chunk.css"
+  },
+  {
+    "revision": "5581d36e76261ad5e333",
+    "url": "/beta-touhou-friends/static/js/main.5581d36e.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/beta-touhou-friends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "ae4ffcf8a45888828ac1",
+    "url": "/beta-touhou-friends/static/css/2.cf62b11c.chunk.css"
+  },
+  {
+    "revision": "ae4ffcf8a45888828ac1",
+    "url": "/beta-touhou-friends/static/js/2.ae4ffcf8.chunk.js"
+  },
+  {
+    "revision": "a54a740d0b6520a5994b",
+    "url": "/beta-touhou-friends/static/js/runtime~main.a54a740d.js"
+  },
+  {
+    "revision": "effea683ab9c6a202995f87a16aaabc3",
+    "url": "/beta-touhou-friends/static/media/Parquetipo-Unicase.effea683.woff"
+  },
+  {
+    "revision": "10304d6823439cd9845e793e8913f3b1",
+    "url": "/beta-touhou-friends/index.html"
+  }
+];
